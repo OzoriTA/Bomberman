726457b3972d27c2b4e582abf2942109f1486d06
OzoriTA <109146030+OzoriTA@users.noreply.github.com>
2023-03-25 12:58:15 +0700

    Viết class BombView

--- src/main/java/bomberman/view/BombView.java
+++ src/main/java/bomberman/view/BombView.java
@@ -1,5 +1,49 @@
 package bomberman.view;
 
-public class BombView {
-    
+
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+public class BombView extends Pane {
+
+    private ImageView bombImage;
+    private int bombSize;
+
+    // Tạo hàm khởi tạo với các tham số
+    public BombView(Image bombImage, int bombSize) {
+        this.bombSize = bombSize;
+
+        bombImage = new ImageView(bombImage);
+        bombImage.setFitWidth(bombSize);
+        bombImage.setFitHeight(bombSize);
+
+        getChildren().add(bombImage);
+    }
+
+
+    public ImageView getBombImage() {
+        return bombImage;
+    }
+
+    public void setBombImage(ImageView imageView) {
+        this.bombImage = imageView;
+    }
+
+    public int getBombSize() {
+        return bombSize;
+    }
+
+    public void setBombSize(int size) {
+        this.bombSize = size;
+        bombImage.setFitWidth(size);
+        bombImage.setFitHeight(size);
+    }
+
+
+    public void updatePosition(int x, int y) {
+        setTranslateX(x * bombSize);
+        setTranslateY(y * bombSize);
+    }
 }
