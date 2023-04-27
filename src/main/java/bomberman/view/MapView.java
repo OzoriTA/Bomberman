7be6c6e6601037540773f5ddfafd42f36413ac24
OzoriTA <109146030+OzoriTA@users.noreply.github.com>
2023-04-27 15:16:18 +0700

    Làm lại toàn bộ

--- src/main/java/bomberman/view/MapView.java
+++ src/main/java/bomberman/view/MapView.java
@@ -1,19 +1,62 @@
 package bomberman.view;
 
+import java.util.ArrayList;
+import java.util.List;
 
 import bomberman.model.Map;
-import javafx.scene.Group;
+import bomberman.model.Position;
+import bomberman.model.Tile;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 
-public class MapView {
-    private Group mapGroup;
+public class MapView extends Pane {
+    private int tileSize;
+    private Map map;
+    private List<List<Rectangle>> tiles;
 
-    public MapView(Map map) {
-        // Initialize map view here
+    public MapView(Map map, int tileSize) {
+        this.map = map;
+        this.tileSize = tileSize;
+        this.tiles = new ArrayList<>();
+
+        for (int i = 0; i < map.getWidth(); i++) {
+            List<Rectangle> column = new ArrayList<>();
+
+            for (int j = 0; j < map.getHeight(); j++) {
+                Rectangle tile = new Rectangle(tileSize, tileSize);
+                tile.setFill(Color.GREEN);
+
+                column.add(tile);
+                getChildren().add(tile);
+            }
+
+            tiles.add(column);
+        }
     }
 
-    public void updateView() {
-        // Update the view of the map here
+    public void updateTile(Position position, Tile tile) {
+        int x = position.getX();
+        int y = position.getY();
+
+        Rectangle rect = tiles.get(x).get(y);
+
+        if (tile.isWall()) {
+            rect.setFill(Color.GRAY);
+        } else if (tile.isDestructible()) {
+            rect.setFill(Color.ORANGE);
+        } else if (tile.containsPowerUp()) {
+            rect.setFill(Color.BLUE);
+        } else {
+            rect.setFill(Color.GREEN);
+        }
     }
 
-    // Getter and setter methods for private fields
+    public void draw() {
+        for (int i = 0; i < map.getWidth(); i++) {
+            for (int j = 0; j < map.getHeight(); j++) {
+                updateTile(new Position(i, j), map.getTileAt(new Position(i, j)));
+            }
+        }
+    }
 }
